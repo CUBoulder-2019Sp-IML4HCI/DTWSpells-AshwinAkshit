24fc4f944e788f12dd51c4d7a2364c70bdd7da45
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2019-02-27 15:06:44 -0700

    initial commit

---
+++ myo_wand.py
@@ -0,0 +1,20 @@
+
+import serial
+from pythonosc import udp_client
+ 
+# change this string depending upon where your computer makes a device for the micro:bit
+serialport = "/dev/cu.usbmodem141402"
+ 
+ser = serial.Serial(serialport, 115200)
+client = udp_client.SimpleUDPClient("localhost", 8999)
+ 
+while(True):
+    line = ser.readline().decode('utf8').strip()
+    try:
+        x, y, z = map(float, line.split("^"))
+    
+        print(f"({x}, {y}, {z})")
+    
+        client.send_message("/wand_accel", [x, y, z] )
+    except:
+        pass
